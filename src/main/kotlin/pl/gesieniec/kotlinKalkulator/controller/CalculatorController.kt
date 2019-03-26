3f27bea784d1bb08b4821d652bcd997abfcb11e0
Przemyslaw Gesieniec <Przemyslaw.Gesieniec@ttpsc.pl>
2019-03-26 20:41:33 +0100

    post wip

--- src/main/kotlin/pl/gesieniec/kotlinKalkulator/controller/CalculatorController.kt
+++ src/main/kotlin/pl/gesieniec/kotlinKalkulator/controller/CalculatorController.kt
@@ -1,18 +1,33 @@
 package pl.gesieniec.kotlinKalkulator.controller
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
+import pl.gesieniec.kotlinKalkulator.service.CalculationService
 
 
 @Controller
-class GreetingController {
+class CalculatorController {
 
-    @GetMapping("/index")
-    fun greeting(@RequestParam(name = "name", required = false, defaultValue = "World") name: String, model: Model): String {
-        model.addAttribute("name", name)
-        return "greeting"
+    @Autowired
+    lateinit var calculationService: CalculationService
+
+    @GetMapping("/")
+    fun init(): String {
+        return "index"
+    }
+
+    @PostMapping("/calculate")
+    fun calculate(@ModelAttribute("result") result: String, model: Model): String {
+
+        val calculatedValue = calculationService.calculate(result)
+        print("my result: ")
+        print(result)
+        model.addAttribute("result", calculatedValue)
+        return "index"
     }
 
 }
--- src/main/kotlin/pl/gesieniec/kotlinKalkulator/service/CalculationService.kt
+++ src/main/kotlin/pl/gesieniec/kotlinKalkulator/service/CalculationService.kt
@@ -1,2 +1,13 @@
 package pl.gesieniec.kotlinKalkulator.service
 
+import org.springframework.stereotype.Service
+
+@Service
+class CalculationService {
+
+    fun calculate(result: String): Double {
+
+        println(result)
+        return 34.0
+    }
+}
