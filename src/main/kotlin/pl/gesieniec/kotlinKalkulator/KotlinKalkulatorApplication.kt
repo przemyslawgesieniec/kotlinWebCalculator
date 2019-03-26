035e9e1daec13cdf79624509fd1352b9595e850d
Przemyslaw Gesieniec <Przemyslaw.Gesieniec@ttpsc.pl>
2019-03-26 17:52:21 +0100

    init commit

---
+++ src/main/kotlin/pl/gesieniec/kotlinKalkulator/KotlinKalkulatorApplication.kt
@@ -0,0 +1,11 @@
+package pl.gesieniec.kotlinKalkulator
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinKalkulatorApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinKalkulatorApplication>(*args)
+}
---
+++ src/main/kotlin/pl/gesieniec/kotlinKalkulator/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package pl.gesieniec.kotlinKalkulator
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(KotlinKalkulatorApplication::class.java)
+	}
+
+}
